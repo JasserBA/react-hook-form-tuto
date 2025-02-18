f5b56a76d9cfce31d7b3ec540dbea8a1b479b12e
JasserBA <jasserbenabdallah6@gmail.com>
2025-02-18 17:34:05 +0100

    added static form

---
+++ src/components/TutoForm.tsx
@@ -0,0 +1,24 @@
+import { useForm } from "react-hook-form"
+
+
+export const TutoForm = () => {
+  const form = useForm();
+  const {register} = form
+  return (
+    <div>
+        <form >
+            <label htmlFor="username">Username</label>
+            {/* <input type='text' id="username" name={name} ref={ref} onChange={onChange} onBlur={onBlur} /> */}
+            <input type='text' id="username" {...register("username")} />
+
+            <label htmlFor="email">Email</label>
+            <input type='text' id="email" {...register("email")}/>
+            
+            <label htmlFor="channel">Channel</label>
+            <input type='text' id="channel" {...register("channel")}/>
+
+            <button>Submit</button>
+        </form>
+    </div>
+  )
+}
