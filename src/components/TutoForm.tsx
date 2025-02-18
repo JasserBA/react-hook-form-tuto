2e20af6217a83426aa7e8c0ff22a338172f4f5d0
JasserBA <jasserbenabdallah6@gmail.com>
2025-02-18 17:47:35 +0100

    added a DevTools to clearly see tracking field value.

--- src/components/TutoForm.tsx
+++ src/components/TutoForm.tsx
@@ -1,9 +1,9 @@
 import { useForm } from "react-hook-form"
-
+import {DevTool} from "@hookform/devtools"
 
 export const TutoForm = () => {
   const form = useForm();
-  const {register} = form
+  const {register, control} = form
   return (
     <div>
         <form >
@@ -19,6 +19,10 @@ export const TutoForm = () => {
 
             <button>Submit</button>
         </form>
+         
+         {/* DevTools to able clearly see that the library is tracking the field values! */}
+         {/* control prop should be added, and assigned control object a value from form  */}
+        <DevTool control={control}/>
     </div>
   )
 }
